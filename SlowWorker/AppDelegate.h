ea1b5fa8c21de6bbe0dd702f69afd4cac4ba2960
王慧勇 <weifeng19911024@163.com>
2017-01-29 17:44:54 +0800

    Initial Commit

---
+++ SlowWorker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlowWorker
+//
+//  Created by wanghuiyong on 29/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlowWorker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlowWorker
+//
+//  Created by wanghuiyong on 29/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
